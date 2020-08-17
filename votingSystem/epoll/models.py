7b66c9318d20654439bc9c269a6b6acf62072589
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2020-08-17 23:54:26 +0600

    in the process

--- votingSystem/epoll/models.py
+++ votingSystem/epoll/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Position(models.Model):
+	title = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return f"{self.title}"
+
+
+class Candidate(models.Model):
+	name = models.CharField(max_length=50)
+	no_votes=models.IntegerField(default=0, editable=False)
+	position = models.ForeignKey(Position, on_delete=models.CASCADE)
+	image = models.ImageField(verbose_name="Candidate Picture", upload_to='images/')
+
+	def __str__(self):
+		return f'{self.name} - {self.position.title}'
+
+
+class VoteStatus(models.Model):
+	name=models.ForeignKey(User, on_delete=models.CASCADE)
+	position=models.ForeignKey(Position, on_delete=models.CASCADE)
+	status=models.BooleanField(default=False)
+
+	def __str__(self):
+		return f"{self.name} - {self.position.title} - {self.status}"
