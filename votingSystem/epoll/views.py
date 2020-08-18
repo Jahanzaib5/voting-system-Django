174d5269105ee371be0a5318d4a2cbc4d8749648
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2020-08-18 17:59:16 +0600

    in the process

--- votingSystem/epoll/views.py
+++ votingSystem/epoll/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404,redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login,logout,authenticate, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from .models import Candidate, VoteStatus,Position
 
 # Create your views here.
+def homepage(request):
+	return render(request, "epoll/home.html")
+
 def index(request):
 	return HttpResponse('Hello world')
